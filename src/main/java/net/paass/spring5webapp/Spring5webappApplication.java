37472d655580f17b474a6cec38ab2cbb75674578
Sven Paaß <sven@paass.net>
2019-03-30 10:56:23 +0100

    add JPA and H2 Console

--- src/main/java/net/paass/spring5webapp/Spring5webappApplication.java
+++ src/main/java/net/paass/spring5webapp/Spring5webappApplication.java
@@ -1,7 +1,11 @@
 package net.paass.spring5webapp;
 
+import org.apache.catalina.servlets.WebdavServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Spring5webappApplication {
