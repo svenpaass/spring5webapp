f82e738dea07fcbe2cf18d4454c49d96948087a9
Sven Paaß <sven@paass.net>
2019-03-30 11:06:02 +0100

    equals / hashCode and toString added
    - equals / hashCode methods for testing equality by id. (needed by Set)
    - toString method for outputting current object values

--- src/main/java/net/paass/spring5webapp/model/Author.java
+++ src/main/java/net/paass/spring5webapp/model/Author.java
@@ -1,6 +1,7 @@
 package net.paass.spring5webapp.model;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -63,5 +64,34 @@ public class Author {
 
   public void setBooks(Set<Book> books) {
     this.books = books;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Author author = (Author) o;
+
+    return id != null ? id.equals(author.id) : author.id == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return id != null ? id.hashCode() : 0;
+  }
+
+  @Override
+  public String toString() {
+    return "Author{" +
+        "id=" + id +
+        ", firstName='" + firstName + '\'' +
+        ", lastName='" + lastName + '\'' +
+        ", books=" + books +
+        '}';
   }
 }
